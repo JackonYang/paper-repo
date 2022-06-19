48f7dc924672b0d9aa1498703979baba6d2abf09
Jackon Yang <i@jackon.me>
2022-06-20 02:00:07 +0800

    gen pdf title

--- code/manage.py
+++ code/manage.py
@@ -1,8 +1,27 @@
-from pdf_pipeline import check_duplicate_pdf
+import os
+from pdf_pipeline import (
+    # check_duplicate_pdf,
+    parse_pdf_title,
+)
+
+from pdf_pipeline import utils
+from pdf_pipeline.configs import PDF_DIR, PROJ_DIR
 
 
 def main():
-    check_duplicate_pdf.main()
+    pdf_files = utils.get_file_list(PDF_DIR, '.pdf')
+
+    data = []
+
+    # check_duplicate_pdf.main(pdf_files)
+    titles = parse_pdf_title.parse_list(pdf_files)
+    for pdf_path, title in titles.items():
+        data.append({
+            'pdf_path': os.path.relpath(pdf_path, PROJ_DIR),
+            'title': title,
+        })
+
+    print(data[0])
 
 
 if __name__ == '__main__':
