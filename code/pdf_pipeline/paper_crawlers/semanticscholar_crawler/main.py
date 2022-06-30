b928dea298520ef0520dc29afc2b57696e99f7fd
Jackon Yang <i@jackon.me>
2022-07-01 01:22:37 +0800

    dig alg refine

--- code/pdf_pipeline/paper_crawlers/semanticscholar_crawler/main.py
+++ code/pdf_pipeline/paper_crawlers/semanticscholar_crawler/main.py
@@ -10,14 +10,28 @@ CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 url_ptn = 'https://www.semanticscholar.org/paper/%s/%s?sort=total-citations'
 
 
-def append_url(urls, url):
+def append_url_by_string(urls, url, drop_exists=False):
+    url = url.strip()
     base_url = url.split('?', 1)[0]
     pid = base_url.split('/')[-1]
 
     outfile = os.path.join(REFERENCE_INFO_DIR, 'ref-info-%s.json' % pid)
+
+    if drop_exists and os.path.exists(outfile):
+        return
+
     urls.append([pid, url, outfile])
 
-    return urls
+
+def append_url_by_dict(urls, ref):
+    if 'id' not in ref or 'slug' not in ref:
+        return
+
+    pid = ref['id']
+    url = url_ptn % (ref['slug'], pid)
+    outfile = os.path.join(REFERENCE_INFO_DIR, 'ref-info-%s.json' % pid)
+
+    urls.append([pid, url, outfile])
 
 
 def read_urls_from_file(file_path):
@@ -26,7 +40,7 @@ def read_urls_from_file(file_path):
 
     urls = []
     for url in data:
-        append_url(urls, url)
+        append_url_by_string(urls, url)
     return urls
 
 
@@ -38,15 +52,7 @@ def read_urls_from_refs(dir_path):
         with open(os.path.join(dir_path, file_path), 'r') as f:
             data = json.load(f)
         for ref in data['links']:
-            if 'id' not in ref or 'slug' not in ref:
-                continue
-
-            pid = ref['id']
-            url = url_ptn % (ref['slug'], pid)
-            outfile = os.path.join(REFERENCE_INFO_DIR, 'ref-info-%s.json' % pid)
-
-            urls.append([pid, url, outfile])
-
+            append_url_by_dict(urls, ref)
     return urls
 
 
@@ -61,18 +67,22 @@ def download_urls(urls):
         pid, url, outfile = task
 
         if os.path.exists(outfile):
-            continue
+            with open(outfile, 'r') as fr:
+                data = json.load(fr)
+            new_ref_links = data['links']
+        else:
+            data = download_ref_links(pid, url, outfile)
+            new_ref_links = data['links']
+            print('(%s/%s)%s refs downloaded. url: %s' % (
+                idx + 1, len(urls), len(new_ref_links), url))
 
-        data = download_ref_links(pid, url, outfile)
-        new_ref_links = data['links']
-        ref_urls.extend(new_ref_links)
-        print('(%s/%s)%s refs downloaded. url: %s' % (
-            idx + 1, len(urls), len(new_ref_links), url))
+        for ref in new_ref_links:
+            append_url_by_dict(ref_urls, ref)
 
     return ref_urls
 
 
-def filter_valuable_urls(urls, min_count=5):
+def filter_valuable_urls(urls, min_count=5, drop_exists=False):
     # count occurence of each url
     url_count = {}
     for task in urls:
@@ -87,26 +97,32 @@ def filter_valuable_urls(urls, min_count=5):
     new_urls = []
     for url in url_count:
         if url_count[url] > min_count:
-            append_url(new_urls, url)
+            append_url_by_string(new_urls, url, drop_exists=drop_exists)
     return new_urls
 
 
 def demo():
     list_file = os.path.join(CUR_DIR, 'url.list')
     urls = read_urls_from_file(list_file)
-    download_urls(urls)
 
-    pre_count = 0
+    loop_cnt = 3
+    seed_distance = 0
+
+    while seed_distance < loop_cnt and len(urls) > 0:
+        print('seed_distance: %s, urls to download: %s' % (
+            seed_distance, len(urls)))
+        new_ref_links = download_urls(urls)
+        urls = filter_valuable_urls(new_ref_links, seed_distance, drop_exists=False)
+
+        seed_distance += 1
 
     while True:
         urls2 = read_urls_from_refs(REFERENCE_INFO_DIR)
-        chosen_urls = filter_valuable_urls(urls2)
+        chosen_urls = filter_valuable_urls(urls2, drop_exists=True)
 
-        cur_cnt = len(chosen_urls)
-        if cur_cnt == pre_count:
+        if len(chosen_urls) == 0:
             break
 
-        pre_count = cur_cnt
         print("orig count: %s, chosen count: %s" % (len(urls2), len(chosen_urls)))
         download_urls(chosen_urls)
 
